3a045b4bdde560454d9d70567f541d9d6a8f38c8
Jonathan <jon_mcdevitt@hotmail.com>
2015-07-08 22:22:59 -0400

    - Created Bucket class - Added some detailed comments

--- BucketSortPrototypeAlpha/src/src/BucketSortPrototypeAlpha.java
+++ BucketSortPrototypeAlpha/src/src/BucketSortPrototypeAlpha.java
@@ -5,6 +5,8 @@
  */
 package src;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Jonathan
@@ -18,4 +20,61 @@ public class BucketSortPrototypeAlpha {
         // TODO code application logic here
     }
     
+}
+
+/**
+ * Attributes:
+ *      ID - must be a single digit, 0-9. Determines which item is sorted in,
+ *          based on the item's hashcode MOD 10.
+ *      size - the size of the bucket, i.e. the number of elements within
+ *      bucket - the bucket itself, container for elements; operates
+ *          similarly to a stack
+ * 
+ * Functions
+ *      Constructors - generates the bucket. Empty constructor never called,
+ *          but exists as a failsafe. Second constructor passes in the relevant
+ *          ID
+ *      Add - drops (adds) an item into the bucket. Increments size.
+ *      Remove - removes an item from the bucket if the element exists within.
+ *          Decrements size and returns this element.
+ *      getID - returns the ID of the bucket.
+ */
+class Bucket<T>{
+    private final int ID;
+    private int size;
+    private final ArrayList<T> bucket;
+    
+    Bucket(){
+        size = 0;
+        ID = 0;
+        bucket = new ArrayList<>();
+    }
+    
+    Bucket(int ID){
+        this.ID = ID;
+        size = 0;
+        bucket = new ArrayList<>();
+    }
+    
+    private void add(T item){
+        bucket.add(item);
+        size++;
+    }
+    private T remove(){
+        T val;
+        if(size>0){
+            val = (bucket.remove(size-1));
+            size--;
+        }
+        else{
+            val = null;
+        }
+        return val;
+    }
+    private int getSize(){
+        return size;
+    }
+    private int getID(){
+        return ID;
+    }
 }
